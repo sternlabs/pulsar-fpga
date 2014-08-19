2583efaa93b796e569dedd27b2842c6d30381626
Simon Schubert <2@0x2c.org>
2014-08-19 11:35:28 +0200

    move pwm multiplex logic into pwm.sv

--- rtl/pwm.sv
+++ rtl/pwm.sv
@@ -1,32 +1,51 @@
 module pwm
-#(parameter pwm_width = 16)
+  #(parameter pwm_width = 16,
+    parameter num_pwm = 4
+)
 (
- input wire                 clk,
- input wire [pwm_width-1:0] counter,
- input wire                 overflow,
- input wire [pwm_width-1:0] new_thres,
- input wire                 set_thres,
- output logic               pwm_out
+ input wire                       clk,
+ input wire [pwm_width-1:0]       new_thres,
+ input wire [$clog2(num_pwm)-1:0] sel_thres,
+ input wire                       set_thres,
+ output logic [num_pwm-1:0]       pwm_out
  );
+
+
+   logic [num_pwm-1:0]      pwm_sel_onehot;
+
+   logic [pwm_width-1:0]    counter;
+   logic                    overflow;
+
+   genvar                   pwm_id;
+
+
+assign pwm_sel_onehot = 1 << sel_thres;
+
+
+counter #(.counter_width(pwm_width)) counter_i(.*);
+
+for (pwm_id = 0; pwm_id < num_pwm; ++pwm_id)
+begin
 
    logic [pwm_width-1:0]    thres;
    logic                    pwm_match;
 
+   // manually calculate comparison to avoid the default comparator
+   // implementation, which uses carry chains and overflows our fpga.
+   assign pwm_match = ~ |(counter ^ thres);
 
-// manually calculate comparison to avoid the default comparator
-// implementation, which uses carry chains and overflows our fpga.
-assign pwm_match = ~ |(counter ^ thres);
-
-always_ff @(posedge clk)
-  if (overflow & set_thres)
-    thres <= new_thres;
+   always_ff @(posedge clk)
+     if (overflow & set_thres & pwm_sel_onehot[pwm_id])
+       thres <= new_thres;
 
 always_ff @(posedge clk)
 begin
    if (overflow)
-     pwm_out <= 1;
+     pwm_out[pwm_id] <= 1;
    if (pwm_match)
-     pwm_out <= 0;
+     pwm_out[pwm_id] <= 0;
+end
+
 end
 
 endmodule
--- rtl/top.sv
+++ rtl/top.sv
@@ -20,7 +20,6 @@ function integer roundup8;
    end
 endfunction
 
-
 localparam pwm_bits = $clog2(num_pwm);
 localparam spi_data_bits = roundup8($clog2(pwm_width));
 localparam spi_width = spi_data_bits + roundup8(pwm_bits);
@@ -29,26 +28,15 @@ localparam spi_width = spi_data_bits + roundup8(pwm_bits);
    logic                    spi_valid;
 
    logic [pwm_width-1:0]    pwm_thres;
-   logic [pwm_bits-1:0]     pwm_sel_bin;
-   logic [num_pwm-1:0]      pwm_sel_onehot;
-
-   logic [pwm_width-1:0]    counter;
-   logic                    overflow;
-
-   genvar                   pwm_id;
+   logic [pwm_bits-1:0]     pwm_sel;
 
 
-assign pwm_sel_bin = spi_data[spi_width-1:spi_width-spi_data_bits];
-assign pwm_sel_onehot = 1 << pwm_sel_bin;
+assign pwm_sel = spi_data[spi_width-1:spi_width-spi_data_bits];
 assign pwm_thres = spi_data[pwm_width-1:0];
 
+
 spi_slave #(.width(spi_width)) spi(.*, .data_ready(spi_valid), .shiftreg(spi_data), .reset(0));
-counter #(.counter_width(pwm_width)) counter_i(.*);
 
-
-for (pwm_id = 0; pwm_id < num_pwm; ++pwm_id)
-begin
-  pwm #(.pwm_width(pwm_width)) pwm(.*, .new_thres(pwm_thres), .set_thres(pwm_sel_onehot[pwm_id]), .pwm_out(pwm_out[pwm_id]));
-end
+pwm #(.pwm_width(pwm_width), .num_pwm(num_pwm)) pwm_i(.*, .new_thres(pwm_thres), .set_thres(spi_valid), .sel_thres(pwm_sel));
 
 endmodule
