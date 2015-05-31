7806a55f6360bdcd67c8204fc303c6dcfc3b3d45
Simon Schubert <2@0x2c.org>
2015-05-31 22:56:10 +0200

    complete top

--- rtl/top.sv
+++ rtl/top.sv
@@ -1,15 +1,13 @@
 module top
-  #(
-    parameter pwm_width = 16,
-    parameter num_pwm = 12
-    )
-(
- input wire                 nCS,
- input wire                 SCK,
- input wire                 MOSI,
+  (
+   nCS,
+   SCK,
+   MOSI,
 
- output logic [num_pwm-1:0] pwm_out
- );
+   pwm_out
+   );
+parameter pwm_width = 16;
+parameter num_pwm = 12;
 
 function integer roundup8;
    input integer            bits;
@@ -18,47 +16,55 @@ function integer roundup8;
    end
 endfunction
 
-localparam pwm_bits = $clog2(pwm_width);
-localparam spi_data_bits = roundup8(pwm_bits);
-localparam spi_width = spi_data_bits + roundup8(num_pwm);
+localparam spi_data_bits = roundup8(pwm_width);
+localparam spi_width = spi_data_bits;
 
-   logic [spi_width-1:0]    spi_data;
-   logic                    spi_valid;
 
-   logic                    clk;
-   logic                    rst;
+   input wire                 nCS;
+   input wire                 SCK;
+   input wire                 MOSI;
 
-   logic                    latch_mem;
-   logic [pwm_bits-1:0]     pwm_addr;
-   logic [num_pwm-1:0]      pwm_data;
+   output logic [num_pwm-1:0] pwm_out;
+
+   logic                      clk;
+   logic                      rst;
+
+   logic [spi_width-1:0]      spi_data;
+   logic                      new_transfer;
+   logic                      transfer_done;
+   logic                      data_ready;
+
+   logic [$clog2(num_pwm)-1:0] write_addr;
+   logic [num_pwm-1:0]         write_data;
+   logic                       write_enable;
+   logic                       write_done;
+
+   logic [$clog2(num_pwm)-1:0] read_addr;
+   logic [num_pwm-1:0]         read_data;
+   logic                       read_latch;
 
 
 platform platform(.*);
 
 spi_slave #(.width(spi_width))
-spi(.data_ready(spi_valid),
+spi(.reset(rst),
     .shiftreg(spi_data),
-    .reset(rst),
     .*);
 
+regwrite #(.width(num_pwm),
+           .num_reg(pwm_width))
+regwrite_i(.data_in(spi_data[num_pwm-1:0]),
+           .*);
 
-   logic [pwm_bits-1:0]     spi_thres_id;
-   logic [num_pwm-1:0]      spi_thres_val;
-
-assign spi_thres_id = spi_data[spi_width-1:spi_data_bits];
-assign spi_thres_val = spi_data[pwm_width-1:0];
-
-thresmem #(.pwm_width(pwm_width),
-           .num_pwm(num_pwm))
-mem(.write_enable(spi_valid),
-    .waddr(spi_thres_id),
-    .wdata(spi_thres_val),
-    .raddr(pwm_addr),
-    .rdata(pwm_data),
-    .*);
+mux #(.width(num_pwm),
+      .num_reg(pwm_width))
+mux_i(.*);
 
 pwm #(.pwm_width(pwm_width),
       .num_pwm(num_pwm))
-pwm_i(.*);
+pwm_i(.pwm_addr(read_addr),
+      .pwm_data(read_data),
+      .latch_mem(read_latch),
+      .*);
 
 endmodule
